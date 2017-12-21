e19e7ecaf8ced922c7df3917fee52b0d767a7742
mparker <mparker@pivotal.io>
2017-12-21 09:16:57 -0500

    first test passing and refactored

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -1,5 +1,61 @@
-describe("foo", function () {
-    it("bars", function () {
-        expect(true).toBe(false)
+const ReactDOM = require("react-dom")
+const React = require("react")
+
+class PlayForm extends React.Component {
+    constructor(){
+        super()
+
+        this.state = {}
+    }
+
+    handleSubmit(){
+        this.setState({status: "INVALID"})
+    }
+
+    render(){
+        return <button onClick={this.handleSubmit.bind(this)}>{this.state.status}</button>
+    }
+}
+
+describe("play round form", function () {
+    describe("playRound requests processes as invalid", function () {
+        beforeEach(function () {
+            requests = { play(p1, p2, observer){ observer.invalid() }}
+        })
+
+        it("display INVALID to the user", function () {
+            renderForm()
+
+            expect(page()).not.toContain("INVALID")
+            submitForm()
+            expect(page()).toContain("INVALID")
+        })
+
     })
+
+    let domFixture, requests
+
+    function setupDom() {
+        domFixture = document.createElement("div")
+        document.body.appendChild(domFixture)
+    }
+
+    beforeEach(function () {
+        setupDom()
+    })
+
+    function renderForm() {
+        ReactDOM.render(
+            <PlayForm requests={requests}/>,
+            domFixture
+        )
+    }
+
+    function page() {
+        return domFixture.innerText;
+    }
+
+    function submitForm() {
+        document.querySelector("button").click()
+    }
 })
