5c9b018d55c518073b27925d901929da3ac6435f
mparker <mparker@pivotal.io>
2017-12-21 09:56:34 -0500

    not passing

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -1,5 +1,6 @@
 const ReactDOM = require("react-dom")
 const React = require("react")
+const ReactTestUtils = require("react-dom/test-utils")
 
 class PlayForm extends React.Component {
     constructor(){
@@ -20,8 +21,25 @@ class PlayForm extends React.Component {
         this.setState({status: "TIE"})
     }
 
+    p1Wins(){
+        this.setState({status: "P1 WINS!!!"})
+    }
+    
+    p2Wins(){
+        this.setState({status: "P2 WINS!!!"})
+    }
+
+    p1ThrowInputChanged(e){
+        this.setState({p1Throw: e.target.value})
+    }
+
     render(){
-        return <button onClick={this.handleSubmit.bind(this)}>{this.state.status}</button>
+        return <div>
+            <input type="text" name="p1Throw" onChange={this.p1ThrowInputChanged.bind(this)}/>
+            <input type="text" name="p2Throw"/>
+
+            <button onClick={this.handleSubmit.bind(this)}>{this.state.status}</button>
+            </div>
     }
 }
 
@@ -55,6 +73,59 @@ describe("play round form", function () {
         })
 
     })
+    
+    describe("playRound requests processes as p1 wins", function () {
+        beforeEach(function () {
+            requests = { play(p1, p2, observer){ observer.p1Wins() }}
+        })
+
+        it("display P1 Wins!!! to the user", function () {
+            renderForm()
+
+            expect(page()).not.toContain("P1 WINS!!!")
+            submitForm()
+            expect(page()).toContain("P1 WINS!!!")
+        })
+
+    })
+    describe("playRound requests processes as p2 wins", function () {
+        beforeEach(function () {
+            requests = { play(p1, p2, observer){ observer.p2Wins() }}
+        })
+
+        it("display P2 Wins!!! to the user", function () {
+            renderForm()
+
+            expect(page()).not.toContain("P2 WINS!!!")
+            submitForm()
+            expect(page()).toContain("P2 WINS!!!")
+        })
+
+    })
+
+    it("sends the user's input to the high level policy", function () {
+        let playSpy = jasmine.createSpy()
+        
+        requests = { play: playSpy}
+        
+        renderForm()
+        
+        let input
+        
+        input = document.querySelector("[name='p1Throw']")
+        input.value = "foo"
+        ReactTestUtils.Simulate.change(input)
+        
+        input = document.querySelector("[name='p2Throw']")
+        input.value = "bar"
+        ReactTestUtils.Simulate.change(input)
+
+        submitForm()
+
+        expect(playSpy).toHaveBeenCalledWith("foo", "bar", jasmine.any(Object))
+    })
+
+
     let domFixture, requests
 
     function setupDom() {
