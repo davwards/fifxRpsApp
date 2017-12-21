9d82b8c663d07e1c7e24bcd08a8833ca4f0cdadf
mparker <mparker@pivotal.io>
2017-12-21 11:19:26 -0500

    pulling Requests out into src folder

--- rps/spec/playSpec.js
+++ rps/spec/playSpec.js
@@ -1,44 +1,4 @@
-function Requests(){
-    this.playRound = function(p1Throw, p2Throw, observer){
-        new PlayRoundRequest(p1Throw, p2Throw, observer).process()
-    }
-}
-
-function PlayRoundRequest(p1Throw, p2Throw, observer){
-    this.process = function(){
-        if (invalidThrow(p1Throw) || invalidThrow(p2Throw))
-            observer.invalid()
-        else if (tie())
-            observer.tie()
-        else if (p1Wins())
-            observer.p1Wins()
-        else
-            observer.p2Wins()
-    }
-
-    function tie() {
-        return p1Throw === p2Throw
-    }
-
-    function p1Wins() {
-        return (
-            p1Throw === ROCK     && p2Throw === SCISSORS  ||
-            p1Throw === SCISSORS && p2Throw === PAPER     ||
-            p1Throw === PAPER    && p2Throw === ROCK
-        )
-    }
-
-    function invalidThrow(theThrow) {
-        return !VALID_THROWS.includes(theThrow)
-    }
-
-
-    const ROCK = "rock"
-    const PAPER = "paper"
-    const SCISSORS = "scissors"
-
-    const VALID_THROWS = [ROCK, PAPER, SCISSORS]
-}
+const { Requests } = require("../src/rps")
 
 describe("play round", function () {
     let observer, requests
